24fdc5c1a88d79ecfb5649dda52776b382911dec
unknown <tylerjoshuahilbert@gmail.com>
2016-03-27 21:08:35 -0400

    Fixed resistance in parallel

--- Multiplier.h
+++ Multiplier.h
@@ -1,6 +1,8 @@
 #ifndef _Multiplier_H
 #define _Multiplier_H
 
+
+// TODO: consider removing this class due to unnecessary complexity compared to small memory benifit.
 class Multiplier {
 public:
 	Multiplier(const char exp = 0);
--- Parallel.cpp
+++ Parallel.cpp
@@ -1,5 +1,5 @@
 #include "Parallel.h"
 
 unsigned long long Parallel::getResistance(Resistor &r1, Resistor &r2) {
-	return 1 / (1 / r1.getResistance() + 1 / r2.getResistance());
+	return 1.0 / (1.0 / r1.getResistance() + 1.0 / r2.getResistance());
 }
