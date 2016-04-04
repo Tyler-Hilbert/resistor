2ff41a9c5cd068338f89719ee1bd613da97bc091
unknown <tylerjoshuahilbert@gmail.com>
2016-04-04 19:58:03 -0400

    Fixed off by 1 multiplier bug

--- Multiplier.cpp
+++ Multiplier.cpp
@@ -16,7 +16,7 @@ Multiplier::Multiplier(const char exp) {
 //}
 
 unsigned long long Multiplier::getMultiplier() {
-	return pow(10, exponent);
+	return pow(10, exponent) + .5; // Add .5 to fix under by 1 truncate bug
 }
 
 void Multiplier::setExponent(const char exp) {
