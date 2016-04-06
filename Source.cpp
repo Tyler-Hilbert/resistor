023ff45d9e9002b23ee68c14e6c07710405c4bd8
Tyler Hilbert <tylerjoshuahilbert@gmail.com>
2016-04-05 22:36:12 -0400

    Removed old code

--- Source.cpp
+++ Source.cpp
@@ -25,24 +25,6 @@ int main() {
 
 	Parallel p;
 	cout << "Resistance in parallel: " << to_string(p.getResistance(resistors)) << "ohms\n";
-
-	// Todo: Get input from iostream
 	
-	
-	//Todo: add this to unit test
-	//Multiplier m0(0);
-	//Multiplier m1(1);
-	//Multiplier m2(-1);
-	//Multiplier m3(3);
-	//Multiplier m4(-2);
-	//BigNum bn0 = m0.getMultiplier();
-	//BigNum bn1 = m1.getMultiplier();
-	//BigNum bn2 = m2.getMultiplier();
-	//BigNum bn3 = m3.getMultiplier();
-	//BigNum bn4 = m4.getMultiplier();
-
-	//cout << "BN0 EXP0:" << bn0.toString() << "\nBN1 Exp1:" << bn1.toString() << "\nexp-1BN2:" << bn2.toString() << "\nBN3 exp3:" << bn3.toString();
-	//cout << bn4.toString();
-
 	return 1;
 }
